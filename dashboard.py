7ca516946959988b79186ccf7c202b70087b233b
Carlos Camacho <carlos010804@gmail.com>
2025-10-14 23:18:06 -0700

    updated assignment14

--- dashboard.py
+++ dashboard.py
@@ -3,10 +3,9 @@ import pandas as pd
 import sqlite3
 
 @st.cache_data
-
 def load_data():
     conn = sqlite3.connect('baseball.db')
-    df = pd.read_sql('Select * FROM events', conn)
+    df = pd.read_sql('Select * FROM get_data', conn)
     conn.close()
 
     return df
@@ -20,11 +19,11 @@ years = sorted(df['Year'].unique())
 selected_year = st.selectbox('Select Year', years)
 filtered = df[df['Year'] == selected_year]
 
-st.subheader('Events in', selected_year)
+st.subheader(f'Statistics in {selected_year}')
 st.dataframe(filtered)
 
-category_counts = filtered['Category'].value_counts().reset_index()
-category_counts.columns = ['Category', 'Count']
+stat_counts = filtered['Stat'].value_counts().reset_index()
+stat_counts.columns = ['Stat', 'Count']
 
-st.bar_chart(category_counts.set_index('Category'))
+st.bar_chart(stat_counts.set_index('Stat'))
 st.write(df.head())
